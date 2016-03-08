1d99e48fb6d6216d19eaf0335007a5aa3c1490dd
Malkollm <fatkullin@bk.ru>
2016-03-08 22:10:57 +0300

    1 задание

---
+++ LAB_4/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Random;
+
+/**
+ * Created by Марсель on 08.03.2016.
+ */
+public class Main {
+
+    public static int rand_numb(int a, int b) {
+        //а и b диапазон чисел, c рандомное целое число
+        Random random = new Random();
+
+        //с = от 1 до 4 (рандомно)
+        int c = a + random.nextInt(b);
+
+        int[] array = new int[20];
+        System.out.println("\n Массив выведенный через метод: ");
+        for (int i = 0; i < array.length; i++) {
+            //массив = от 1 до 4 (рандомно)
+            array[i] = a + random.nextInt(b);
+            System.out.print(array[i] + " ");
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int number = rand_numb(1, 4);
+        System.out.println("\n Теперь выводим число: " + number);
+    }
+}
