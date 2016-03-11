14dc789a93880f047692ace23281418d1953fd6d
Malkollm <fatkullin@bk.ru>
2016-03-11 20:53:27 +0300

    Начало 2 задания

---
+++ LAB_4/src/com/company/Example2.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Random;
+
+/**
+ * Created by Марсель on 11.03.2016.
+ */
+public class Example2 {
+
+    //заполняет массивы
+    public static int rand_massiv(int a) {
+        //а и b диапазон чисел, c рандомное целое число
+        Random random = new Random();
+
+        int[] array = new int[20];
+        System.out.println("\n Массив выведенный через метод: ");
+        for (int i = 0; i < array.length; i++) {
+            //массив = от 1 до 4 (рандомно)
+            array[i] = a + random.nextInt(b);
+            System.out.print(array[i] + " ");
+        }
+        return ;
+    }
+
+    //выводит массивы
+    public static int[] Output_result(){
+        Random random = new Random();
+
+        int[] array = new int [10];
+        for (int i=0;i<array.length;i++){
+            array[i] = random.nextInt(15);
+            System.out.print(array[i]+" ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
