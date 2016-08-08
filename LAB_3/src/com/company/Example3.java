8e0e60d92a2da2282a5e329a222a81c4b4d824e6
Malkollm <fatkullin@bk.ru>
2016-08-08 18:50:04 +0300

    исправления

--- LAB_3/src/com/company/Example3.java
+++ LAB_3/src/com/company/Example3.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import java.util.Random;
-
 /**
  * 10.02.16
  * Фаткуллин Марсель
@@ -9,21 +7,35 @@ import java.util.Random;
  */
 public class Example3 {
     public static void main(String[] args) {
-        int count=0; //Переменная для подсчета количества
+        int count = 0; //Переменная для подсчета количества
+        int cot = 0;
         int[] arr = new int[10];
 
         System.out.print("arr: ");
-        for (int i=0; i<arr.length; i++) {
-            arr[i] = (int)(Math.random() * 10 ); //[0;9]
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10); //[0;9]
             System.out.print(arr[i] + " ");
         }
         //Поиск четных чисел
-        for(int i=0; i<arr.length; i++) {
+        System.out.println(" ");
+
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
-                count+=1;
+                count += 1;
+
+            }
+        }
+        System.out.println(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                cot++;
+
             }
         }
         System.out.println("");
         System.out.println("Количество четных " + count);
+        System.out.println("Количество нечетных " + cot);
     }
 }
+
